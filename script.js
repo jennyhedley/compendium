d64ce059e9f4ca0569920243523322ab6b9d801c
Glitch (a-compendium) <none>
2021-04-24 06:21:00 +0000

    🛰👑 Updated with Glitch

--- script.js
+++ script.js
@@ -36,7 +36,53 @@ function emailToMum() {
     "G lets her sleep under the covers so now she is totally spoiled",
     "G won't let me have Princess Missy and I miss her",
     "I want my cat though"
-  ];  // set up the counter
+  ];
+
+  const gSent1 = [
+    "G had a gambling relapse",
+    "G has finally given me the cash he owed me and is not calling to beg me to return anymore",
+    "G is finally taking a little responsibility for his role in the breakdown of the relationship",
+    "G is finally starting to accept that I am leaving",
+    "G just bought himself his own car since he's working lots of double shifts and part of the deal when we bought our car together is that I have exclusive use of the car on my work nights",
+    "G left all these tearful messages saying, What have you done to us",
+    "I ran out to move the car and drink coffee and field off calls from G",
+    "I had a very good sales weekend then just as I was about to head home at 4am G called and asked if I could give him a lift home from King Street",
+    "I moved out for good just under two weeks ago",
+    "Every day I deal with G crying in my ear, wanting reconciliation and blaming me for his destruction",
+    "I am feeling better about G since he is accepting the split better and no longer is calling to blame me for ruining his life or threatening to die"
+  ];
+  const gSent2 = [
+    "He says, You don't love me, You don't want to see me, blah blah blah",
+    "I picked him up and pulled over to drop him off and he freaked out",
+    "I promised that we could date once or twice a week as a transitional thing to help him come to terms better",
+    "I had a feeling so I ran into the pub in slippers and bootie shorts and wagged my finger and walked out",
+    "He gets tearful and hopeful but I'm not going back",
+    "He is being surprisingly well behaved",
+    "He lets me come visit Missy",
+    "He screamed obscenities at me and threw things at the car window since I would not go inside",
+    "He still gets into moods and tears, but it's far better now that he is reaching a certain acceptance of my decision",
+    "He was so scary the other night",
+    "He will have to behave in public",
+    "He will have to treat me right in order for me to visit him and continue a friendship-relationship",
+    "He won't let me have the cat here even though he claimed he didn't want her",
+    "He's been good about not calling me and stuff",
+    "He's got tenants so I'm no longer paying rent there",
+    "He's sad and self-pitying but I'm glad he's keeping himself on a good path now",
+    "He's still in love but realises it's over"
+  ];
+  const closingSent = [
+    "I just drove away",
+    "I keep having weird dreams about packing up my stuff from my father's house",
+    "I keep saying I won't marry a gambler",
+    "I finally broke even",
+    "I feel so so sad and sick about it",
+    "I can get used to his new self",
+    "I must have had my most un-fun birthday ever so I request a do-over",
+    "I truly budgeted for having these final six weekends of sales",
+    "I feel like if the liquids go down faster rather than trickle down in that bad way they will shoot past your epiglottis fast enough to not get caught",
+    "I filed his taxes for him"
+  ];
+  // set up the counter
   return (
     "Dear Mum, " +
     getRandomFromList(openingSent) +
@@ -44,6 +90,12 @@ function emailToMum() {
     getRandomFromList(missySent1) +
     ". " +
     getRandomFromList(missySent2) +
+    ". " +
+    getRandomFromList(gSent1) +
+    ". " +
+    getRandomFromList(gSent2) +
+    ". " +
+    getRandomFromList(openingSent) +
     ". " +
     "Love, Jenny Cat"
   );
@@ -57,4 +109,3 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
-
